7b0bac9dc95e405e58dd6fa9466b7d43f1a64568
codeSS25 <90010395+codeSS25@users.noreply.github.com>
2021-11-30 23:46:07 +0100

    added crossover withn weights function into Graphs class

--- src/Graph.java
+++ src/Graph.java
@@ -49,55 +49,88 @@ public class Graph {
         fitness = (double) totalDistance / 1000;
     }
 
-/*    //crossover function
-    public DNA crossover(DNA partner) {
-        DNA child = new DNA(cities.length);
-        int midpoint = new Random().nextInt(cities.length);
-        for (int i = 0; i < cities.length; i++) {
-            if (i > midpoint) child.cities[i] = cities[i];
-            else child.cities[i] = partner.cities[i];
-        }
-        return child;
-    }*/
-
     public Graph crossover(Graph partner) {
         Graph child = new Graph();
+        String[] parent1Cities = cities;
+        String[] parent2Cities = partner.cities;
+        String[] childCities = child.cities;
 
-        for (int i = 0; i < cities.length; i+=2) {
-            int rand = new Random().nextInt(2);
-            String node1 = cities[i];
+        childCities[0] = parent1Cities[0];
 
-            if (childContains(node1, child.cities)) continue;
-            else if (rand == 0) { //getting edge from this
-                if (i != cities.length && !childContains(cities[i+1],child.cities)){
-                    child.cities[i] = node1;
-                    child.cities[i+1] = cities[i+1];
-                }
-                else if (i != 0 && !childContains(cities[i-1],child.cities)) {
-                    child.cities[i] = node1;
-                    child.cities[i+1] = cities[i-1];
-                } else {
+        for (int i = 1; i < parent1Cities.length; i++) {
+            int indexOfLastAddedInParent1 = getIndexOf(childCities[i-1], parent1Cities);
+            int indexOfLastAddedInParent2 = getIndexOf(childCities[i-1], parent2Cities);
 
+            boolean[] availableNodes = new boolean[]{false,false,false,false};
+            String[] cities = new String[4];
+
+            if (indexOfLastAddedInParent1 != 0) {
+                availableNodes[0] = getIndexOf(parent1Cities[indexOfLastAddedInParent1-1], childCities) == -1;
+                cities[0] = parent1Cities[indexOfLastAddedInParent1-1];
+            }
+            if (indexOfLastAddedInParent1 < parent1Cities.length-1) {
+                availableNodes[1] = getIndexOf(parent1Cities[indexOfLastAddedInParent1+1], childCities) == -1;
+                cities[1] = parent1Cities[indexOfLastAddedInParent1+1];
+            }
+            if (indexOfLastAddedInParent2 != 0) {
+                availableNodes[2] = getIndexOf(parent2Cities[indexOfLastAddedInParent2-1], childCities) == -1;
+                cities[2] = parent2Cities[indexOfLastAddedInParent2-1];
+            }
+            if (indexOfLastAddedInParent2 < parent2Cities.length-1) {
+                availableNodes[3] = getIndexOf(parent2Cities[indexOfLastAddedInParent2+1], childCities) == -1;
+                cities[3] = parent2Cities[indexOfLastAddedInParent2+1];
+            }
+
+            boolean allFalse = true;
+            for (boolean b: availableNodes) {
+                if (b) {
+                    allFalse = false;
+                    break;
                 }
             }
-            else if (rand == 0) { //getting edge from this
-                if (i != cities.length && !childContains(cities[i+1],child.cities)){
-                    child.cities[i] = node1;
-                    child.cities[i+1] = cities[i+1];
-                }
-                else if (i != 0 && !childContains(cities[i-1],child.cities)) {
-                    child.cities[i] = node1;
-                    child.cities[i+1] = cities[i-1];
-                } else {
 
+            if (allFalse) {
+                boolean found;
+                do {
+                    found = false;
+                    String node = String.valueOf("abcdefghijklmnopqrst".charAt(new Random().nextInt(parent1Cities.length)));
+                    if(getIndexOf(node,childCities) == -1) {
+                        childCities[i] = node;
+                        found = true;
+                    }
+                } while (!found);
+            }
+            else {
+                int indexOfShortest = 0;
+                int shortestDistance = 300;
+                for (int j = 0; j < 4; j++) {
+                    if (availableNodes[j] && graphData.getDistance(childCities[i-1], cities[j]) < shortestDistance) {
+                            indexOfShortest = j;
+                            shortestDistance = graphData.getDistance(childCities[i-1], cities[j]);
+                    }
+                }
+                
+                switch(indexOfShortest) {
+                    case 0 -> childCities[i] = parent1Cities[indexOfLastAddedInParent1-1];
+                    case 1 -> childCities[i] = parent1Cities[indexOfLastAddedInParent1+1];
+                    case 2 -> childCities[i] = parent2Cities[indexOfLastAddedInParent2-1];
+                    case 3 -> childCities[i] = parent2Cities[indexOfLastAddedInParent2+1];
                 }
             }
         }
-        return child;
+       // System.out.println(String.join(", ", childCities));
+       return child;
     }
 
-    public boolean childContains(String str, String[] child) {
-        for (String s: child) {
+    public static int getIndexOf(String s, String[] array) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] != null && array[i].equals(s)) return i;
+        }
+        return -1;
+    }
+
+    public boolean childCitiesContains(String str, String[] childCities) {
+        for (String s: childCities) {
             if (s.equals(str)) return true;
         }
         return false;
